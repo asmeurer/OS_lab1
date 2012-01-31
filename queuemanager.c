0947022e652e6a1bb65b10639de15dbbc3422dd8
Aaron Meurer <asmeurer@gmail.com>
2012-01-30 18:55:55 -0700

    work

--- queuemanager.c
+++ queuemanager.c
@@ -7,27 +7,61 @@ struct process_control_block {
   int psw;   /* Program status word */
   int page_table;   /* Pagetable info */
   int regs[NUM_REGS];   /* Array of registers */
-  process_control_block *next;
-  process_control_block *prev;
-}
+  struct process_control_block *next;
+  struct process_control_block *prev;
+  int empty;
+};
 
 struct process_control_block process[MAX_PROCESSES];
 
-int size = 0;
-int i = 0
+int i = 0;
+struct process_control_block *head = NULL;
+struct process_control_block *tail = NULL;
 
 int enqueue(int pid, int psw, int page_table, int *regs);
+struct process_control_block *find_nonempty();
+viod init();
 
-int enqueue(int pid, int psw, int page_table, int *regs){
-  if (size == MAX_PROCESSES) {
-    return(-1)
-      } else {
-    /* Enqueue */
-    process_control_block[size].pid = pid;
-    process_control_block[size].psw = psw;
-    process_control_block[size].page_table = page_table;
-    for (i = 0; i < NUM_REGS; i++){
-      process_control_block[size].regs[i] = regs[i];
+void init() {
+  for (i = 0; i < MAX_PROCESSES; i++) {
+  process[i].empty = 0;
+ }
+}
+
+struct process_control_block *find_nonempty() {
+  for (i = 0; i < MAX_PROCESSES; i++) {
+    if (process[i].empty == 1) {
+      return &process[i];
     }
   }
+  return NULL;
+}
+
+int enqueue(int pid, int psw, int page_table, int *regs) {
+  /* Enqueue */
+  struct process_control_block *newprocess = find_nonempty();
+
+  if (!newprocess) {
+    /* The queue if full */
+    return(-1);
+  }
+
+  newprocess->pid = pid;
+  newprocess->psw = psw;
+  newprocess->page_table = page_table;
+  for (i = 0; i < NUM_REGS; i++) {
+      newprocess->regs[i] = regs[i];
+    }
+
+  if (tail) {
+    /* The queue already has elements */
+    tail->prev = newprocess;
+    newprocess->next = tail;
+  } else {
+    /* This is the first element of the queue */
+    head = newprocess;
+    tail = newprocess;
+  }
+
+  return(0);
 }
