2f908423ab90b4f8925cb5c59e3463236f19cef4
Aaron Meurer <asmeurer@gmail.com>
2012-01-31 00:29:51 -0700

    Indent with cc-mode style

--- queuemanager.c
+++ queuemanager.c
@@ -11,13 +11,13 @@
 #define NUM_REGS 3
 #define MAX_PROCESSES 20
 struct process_control_block {
-  int pid;   /* Process ID */
-  int psw;   /* Program status word */
-  int page_table;   /* Pagetable info */
-  int regs[NUM_REGS];   /* Array of registers */
-  struct process_control_block *next;
-  struct process_control_block *prev;
-  int empty;
+    int pid;   /* Process ID */
+    int psw;   /* Program status word */
+    int page_table;   /* Pagetable info */
+    int regs[NUM_REGS];   /* Array of registers */
+    struct process_control_block *next;
+    struct process_control_block *prev;
+    int empty;
 };
 
 struct process_control_block process[MAX_PROCESSES];
@@ -33,129 +33,129 @@ int dequeue();
 int delete(int id);
 
 void init() {
-  int i = 0;
-  for (i = 0; i < MAX_PROCESSES; i++) {
-    clear(process);
-  }
+    int i = 0;
+    for (i = 0; i < MAX_PROCESSES; i++) {
+        clear(process);
+    }
 }
 
 struct process_control_block *find_nonempty() {
-  int i = 0;
-  for (i = 0; i < MAX_PROCESSES; i++) {
-    if (process[i].empty == 1) {
-      return &process[i];
+    int i = 0;
+    for (i = 0; i < MAX_PROCESSES; i++) {
+        if (process[i].empty == 1) {
+            return &process[i];
+        }
     }
-  }
-  return null;
+    return null;
 }
 
 int enqueue(int pid, int psw, int page_table, int *regs) {
-  /* Enqueue */
-  struct process_control_block *newprocess = find_nonempty();
-  int i = 0;
+    /* Enqueue */
+    struct process_control_block *newprocess = find_nonempty();
+    int i = 0;
 
-  if (!newprocess) {
-    /* The queue if full */
-    return(-1);
-  }
+    if (!newprocess) {
+        /* The queue if full */
+        return(-1);
+    }
 
-  newprocess->pid = pid;
-  newprocess->psw = psw;
-  newprocess->page_table = page_table;
-  for (i = 0; i < NUM_REGS; i++) {
-    newprocess->regs[i] = regs[i];
-  }
+    newprocess->pid = pid;
+    newprocess->psw = psw;
+    newprocess->page_table = page_table;
+    for (i = 0; i < NUM_REGS; i++) {
+        newprocess->regs[i] = regs[i];
+    }
 
-  if (tail) {
-    /* The queue already has elements */
-    newprocess->next = tail;
-    tail->prev = newprocess;
-  } else {
-    /* This is the first element of the queue */
-    head = newprocess;
-  }
-  tail = newprocess;
-  newprocess->empty = 0;
+    if (tail) {
+        /* The queue already has elements */
+        newprocess->next = tail;
+        tail->prev = newprocess;
+    } else {
+        /* This is the first element of the queue */
+        head = newprocess;
+    }
+    tail = newprocess;
+    newprocess->empty = 0;
 
-  return(0);
+    return(0);
 }
 
 /*Process must exist for clear function*/
 void clear(struct process_control_block *process){
-  int i = 0;
-  process->pid = 0;
-  process->psw = 0;
-  process->page_table = 0;
-  for (i = 0; i < NUM_REGS; i++) {
-    process->regs[i] = 0;
-  }
-  process->next = null;
-  process->prev = null;
-  /*Set as empty*/
-  process->empty = 1;
+    int i = 0;
+    process->pid = 0;
+    process->psw = 0;
+    process->page_table = 0;
+    for (i = 0; i < NUM_REGS; i++) {
+        process->regs[i] = 0;
+    }
+    process->next = null;
+    process->prev = null;
+    /*Set as empty*/
+    process->empty = 1;
 }
 
 
 int dequeue(){
-  /*If queue is empty*/
-  if (head == null){
-    return -1;
-  }
-  int ret = head->pid;
-  struct process_control_block *temp = head;
-  /*If entry is only one in queue*/
-  if(head->prev == null){
-    head = null;
-    tail = null;
-  }
-  else{
-    /*Reset head pointer*/
-    head = head->prev;
-    head->next = null;
-  }
-  clear(temp);
-  return(ret);
+    /*If queue is empty*/
+    if (head == null){
+        return -1;
+    }
+    int ret = head->pid;
+    struct process_control_block *temp = head;
+    /*If entry is only one in queue*/
+    if(head->prev == null){
+        head = null;
+        tail = null;
+    }
+    else{
+        /*Reset head pointer*/
+        head = head->prev;
+        head->next = null;
+    }
+    clear(temp);
+    return(ret);
 }
 
 struct process_control_block *find_process(int id){
-  struct process_control_block *temp = tail;
-  while(temp != null){
-    if(id == temp->pid){
-      return temp;
+    struct process_control_block *temp = tail;
+    while(temp != null){
+        if(id == temp->pid){
+            return temp;
+        }
+        temp = temp->next;
     }
-    temp = temp->next;
-  }
-  return null;
+    return null;
 }
 
 int delete(int id){
-  struct process_control_block *temp = find_process(id);
-  /*If process doesn't exist*/
-  if (temp == null){
-    return -1;
-  }
-  int ret = temp->pid;
-  /*If entry is only one in queue*/
-  if(temp->next == null && temp->prev == null){
-    head = null;
-    tail = null;
-  }
-  /*If entry is at tail*/
-  else if(temp->prev == null){
-    tail = temp->next;
-    tail->prev = null;
-  }
-  /*If entry is at head*/
-  else if(temp->next == null){
-    head = temp->prev;
-    head->next = null;
-  }
-  /*If entry is in the middle*/
-  else{
-    temp->prev->next = temp->next;
-    temp->next->prev = temp->prev;
-  }
-  clear(temp);
-  return (ret);
+    struct process_control_block *temp = find_process(id);
+    /*If process doesn't exist*/
+    if (temp == null){
+        return -1;
+    }
+    int ret = temp->pid;
+    /*If entry is only one in queue*/
+    if(temp->next == null && temp->prev == null){
+        head = null;
+        tail = null;
+    }
+    /*If entry is at tail*/
+    else if(temp->prev == null){
+        tail = temp->next;
+        tail->prev = null;
+    }
+    /*If entry is at head*/
+    else if(temp->next == null){
+        head = temp->prev;
+        head->next = null;
+    }
+    /*If entry is in the middle*/
+    else{
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+    }
+    clear(temp);
+    return (ret);
 
 }
--- queuemanager_test.c
+++ queuemanager_test.c
@@ -13,110 +13,110 @@ void printprocess(struct process_control_block process);
 void listQ();
 
 void printprocess(struct process_control_block process) {
-  int i = 0;
+    int i = 0;
 
-  printf("\n");
-  printf("pid: %d\n", process.pid);
-  printf("psw: %d\n", process.psw);
-  printf("page_table: %d\n", process.page_table);
-  printf("regs:\n");
-  for (i = 0; i < NUM_REGS; i++) {
-    printf("  %d\n", process.regs[i]);
-  }
-  /* printf("\n"); */
+    printf("\n");
+    printf("pid: %d\n", process.pid);
+    printf("psw: %d\n", process.psw);
+    printf("page_table: %d\n", process.page_table);
+    printf("regs:\n");
+    for (i = 0; i < NUM_REGS; i++) {
+        printf("  %d\n", process.regs[i]);
+    }
+    /* printf("\n"); */
 }
 
 void listQ() {
-  struct process_control_block *temp = head;
-  printf("Start of queue.\n");
-  while (temp) {
-    printprocess(*temp);
-    temp = temp->prev;
-  }
-  printf("\nEnd of queue.\n\n");
+    struct process_control_block *temp = head;
+    printf("Start of queue.\n");
+    while (temp) {
+        printprocess(*temp);
+        temp = temp->prev;
+    }
+    printf("\nEnd of queue.\n\n");
 }
 
 int main() {
-  int i = 0;
-  int r = 0;
-  char line[100];
-  char delim[] = " \n";
-  char *command;
-  char *args[6];
-  int pid = 0;
-  int psw = 0;
-  int page_table = 0;
-  int reg1 = 0;
-  int reg2 = 0;
-  int reg3 = 0;
-  int regs[3];
+    int i = 0;
+    int r = 0;
+    char line[100];
+    char delim[] = " \n";
+    char *command;
+    char *args[6];
+    int pid = 0;
+    int psw = 0;
+    int page_table = 0;
+    int reg1 = 0;
+    int reg2 = 0;
+    int reg3 = 0;
+    int regs[3];
 
-  init();
+    init();
 
-  FILE *file = fopen("tests", "r");
+    FILE *file = fopen("tests", "r");
 
-  while(fgets(line, 100, file) != NULL) {
-    /* printf("reading the file\n"); */
-    command = strtok(line, delim);
+    while(fgets(line, 100, file) != NULL) {
+        /* printf("reading the file\n"); */
+        command = strtok(line, delim);
 
-    /* printf("%s\n", line); */
-    /* printf("%s\n", command); */
+        /* printf("%s\n", line); */
+        /* printf("%s\n", command); */
 
-    if (!strcmp(command, "init_Q")) {
-      /* printf("initializing\n"); */
-      init();
+        if (!strcmp(command, "init_Q")) {
+            /* printf("initializing\n"); */
+            init();
+        }
+        else if (!strcmp(command, "list")) {
+            /* printf("listing\n"); */
+            listQ();
+        }
+        else if (!strcmp(command, "enqueue")) {
+            /* printf("enqueueing\n"); */
+            for (i = 0; i < 6; i++) {
+                args[i] = strtok(NULL, delim);
+            }
+            pid = atoi(args[0]);
+            psw = atoi(args[1]);
+            page_table = atoi(args[2]);
+            reg1 = atoi(args[3]);
+            reg2 = atoi(args[4]);
+            reg3 = atoi(args[5]);
+
+            /* printf("pid: %d, psw: %d, page_table: %d, reg1: %d, reg2: %d, reg3: %d\n", pid, psw, page_table, reg1, reg2, reg3); */
+            regs[0] = reg1;
+            regs[1] = reg2;
+            regs[2] = reg3;
+
+            r = enqueue(pid, psw, page_table, regs);
+            if (r == -1) {
+                printf("Could not enqueue; queue full.");
+            }
+        }
+        else if (!strcmp(command, "dequeue")) {
+            /* printf("dequeueing\n"); */
+            r = dequeue();
+            if (r == -1) {
+                printf("Could not dequeue: queue empty.");
+            } else {
+                printf("%d\n", r);
+            }
+        }
+        else if (!strcmp(command, "delete")) {
+            /* printf("deleting\n"); */
+            pid = atoi(strtok(NULL, delim));
+            r = delete(pid);
+            if (r == -1) {
+                printf("Could not dequeue: queue empty.");
+            } else {
+                printf("%d\n", r);
+            }
+        }
+        else {
+            printf("Unrecognized command: %s\n", command);
+        }
     }
-    else if (!strcmp(command, "list")) {
-      /* printf("listing\n"); */
-      listQ();
-    }
-    else if (!strcmp(command, "enqueue")) {
-      /* printf("enqueueing\n"); */
-      for (i = 0; i < 6; i++) {
-        args[i] = strtok(NULL, delim);
-      }
-      pid = atoi(args[0]);
-      psw = atoi(args[1]);
-      page_table = atoi(args[2]);
-      reg1 = atoi(args[3]);
-      reg2 = atoi(args[4]);
-      reg3 = atoi(args[5]);
 
-      /* printf("pid: %d, psw: %d, page_table: %d, reg1: %d, reg2: %d, reg3: %d\n", pid, psw, page_table, reg1, reg2, reg3); */
-      regs[0] = reg1;
-      regs[1] = reg2;
-      regs[2] = reg3;
+    close(file);
 
-      r = enqueue(pid, psw, page_table, regs);
-      if (r == -1) {
-        printf("Could not enqueue; queue full.");
-      }
-    }
-    else if (!strcmp(command, "dequeue")) {
-      /* printf("dequeueing\n"); */
-      r = dequeue();
-      if (r == -1) {
-        printf("Could not dequeue: queue empty.");
-      } else {
-        printf("%d\n", r);
-      }
-    }
-    else if (!strcmp(command, "delete")) {
-      /* printf("deleting\n"); */
-      pid = atoi(strtok(NULL, delim));
-      r = delete(pid);
-      if (r == -1) {
-        printf("Could not dequeue: queue empty.");
-      } else {
-        printf("%d\n", r);
-      }
-    }
-    else {
-      printf("Unrecognized command: %s\n", command);
-    }
-  }
-
-  close(file);
-
-  return(0);
+    return(0);
 }
