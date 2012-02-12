0e2e8482bab6b0e477217ee25e2ed98fe6bd0098
unknown <Sheng@ShengBurger.(none)>
2012-02-12 15:19:49 -0700

    Created move function in queue manager

--- processmanager.c
+++ processmanager.c
@@ -8,6 +8,31 @@
 
 #include"queuemanager.h"
 
+
+/* struct process_control_block {
+    int pid;
+    int psw;
+    int page_table;
+    int regs[NUM_REGS];
+    struct process_control_block *next;
+    struct process_control_block *prev;
+    int empty;
+}; */
+
 /* NULL is in stdio.h.  Redefining NULL gives a warning, so call it null
    instead. */
 #define null 0
+
+int move(enum queue_enum from_queue, enum queue_enum to_queue){
+	struct process_control_block temp = dequeue(get_process(from_queue));
+	/*Nothing in queue, recoverable*/
+	if(temp.pid == -1){
+		return -1;
+	}
+	int error = enqueue(get_process(to_queue), temp.pid, temp.psw, temp.page_table, temp.regs);
+	/*Queue full, unrecoverable error*/
+	if(error == -1){
+		return -666;
+	}
+	return 0;
+}
