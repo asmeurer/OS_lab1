7522bf70b5b44711a8cae6cad4d126252eaa748d
Sheng <sheng@ubuntu.(none)>
2012-03-19 18:11:58 -0600

    added definition files

--- IPC/ipc.c
+++ IPC/ipc.c
@@ -5,3 +5,17 @@
    Oran Wallace
    Sheng Lundquist
 */
+
+int move(enum QUEUES from_queue, enum QUEUES to_queue){
+    struct process_control_block temp = dequeue(from_queue);
+    /*Nothing in queue, recoverable*/
+    if(temp.pid == ERROR_QUEUE_EMPTY){
+        return ERROR_QUEUE_EMPTY;
+    }
+    int error = enqueue(to_queue, temp.pid, temp.psw, temp.page_table, temp.regs, temp.priority, temp.quantum_count, temp.group);
+    /*Queue full, recoverable error*/
+    if(error == ERROR_QUEUE_FULL){
+        return ERROR_QUEUE_FULL;
+    }
+    return ERROR_SUCCESS;
+}
