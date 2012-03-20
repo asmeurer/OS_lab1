868792925bbdabc0ba20fa01d921e17bbea680e9
Aaron Meurer <asmeurer@gmail.com>
2012-03-19 18:24:09 -0600

    Update the ipc definitions file

--- IPC/ipc_definitions.h
+++ IPC/ipc_definitions.h
@@ -17,6 +17,7 @@
 #define NUM_REGS 3
 #define MAX_PROCESSES 20
 #define MAX_PRIORITY 10
+#define MAX_MESSAGES 5
 
 /*Global Error Codes*/
 #define ERROR_SUCCESS 0
@@ -24,30 +25,37 @@
 #define ERROR_QUEUE_FULL -2
 #define ERROR_QUEUE_NOT_EXIST -3
 
+enum MESSAGE_QUEUES {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE
+}
+    message_queue_enum;
 
-struct message_control_block {
-    int pid;   /* Process ID */
-    int psw;   /* Program status word */
-    int page_table;   /* Pagetable info */
-    int regs[NUM_REGS];   /* Array of registers */
-    struct process_control_block *next;
-    struct process_control_block *prev;
+
+struct message
+{
+    int source;
+    int destination;
+    char string[256];
+    struct message *next;
+    struct message *prev;
     int empty;
-    int priority;
-    int quantum_count;
-    enum QUEUES group;
-};
+}
 
-struct queue_t {
-    struct process_control_block *head;
-    struct process_control_block *tail;
-    int size;
-    struct process_control_block *top;
-};
+    struct queue_message_t
+    {
+        struct message *head;
+        struct message *tail;
+        int size;
+        struct message *top;
+    }
 
-int process_counter;
-int pid_counter;
-int global_quantum_count;
-enum QUEUES current_group;
 
 #endif
