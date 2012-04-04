52164d1da09eda06b6b24639db9bd0f996d3b061
Sheng <sheng@ubuntu.(none)>
2012-04-04 00:35:52 -0600

    Wrote code for test implementor and function for memory hardware page hit

--- memory/memory_hardware.c
+++ memory/memory_hardware.c
@@ -9,5 +9,22 @@
  */
 
 #include "memory_hardware.h"
-
-unsigned int counter = 0;
+int HW_page_hit(int page_table_index, int page_num){
+    //Check first page number init
+    //Will return 0 if 4th bit in bits not set
+    if(!(page_tables[page_table_index][0].bits & P_BITMASK)){
+        return ERROR_PAGE_TABLE_NOT_INIT;
+    }
+    //Check if page number init
+    if(!(page_tables[page_table_index][page_num].bits & P_BITMASK)){
+        return ERROR_PAGE_NOT_INIT;
+    }
+    //Check if that page already exists in phy memory
+    if(!(page_tables[page_table_index][page_num].bits & PMV_BITMASK)){
+        return ERROR_HARDWARE_NOT_IN_PHY_MEM;
+    }
+    byte addr = page_tables[page_table_index][page_num].phy_addr;
+    global_LRU_counter++;
+    phy_mem[addr].LRU = global_LRU_counter;
+    return ERROR_SUCCESS;
+}
