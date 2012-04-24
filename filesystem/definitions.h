6f998e0a9d8b9df1c2afc01662c7579ee507b2e8
Sheng Lundquist <slundqui007@gmail.com>
2012-04-24 17:55:47 -0600

    Added more stuff to definitions

--- filesystem/definitions.h
+++ filesystem/definitions.h
@@ -25,6 +25,9 @@ typedef unsigned char byte;
 #define MAX_OPEN 8
 #define FCB_DIR_BITMASK 0x01
 #define FCB_WRITE_ACC_BITMASK 0x02
+#define DEVICE_FORMAT_BITMASK 0x01
+#define DEVICE_MOUNTED_BITMASK 0x02
+#define NAME_LIMIT 11
 
 /*Buffers*/
 short rBuffers [MAX_DEVICE][BUFFER_SIZE];
@@ -34,8 +37,8 @@ byte wBufSize [MAX_DEVICE];
 
 /*FCB*/
 typedef struct{
-	char filename[10];
-	/*Goes 000000(Write access)(Directory)*/
+	char filename[NAME_LIMIT];
+	/*0 0 0 0 0 0 (Write access) (Directory)*/
 	byte bits;
 	struct fcb* dirHead;
 	short* blockhead;
@@ -43,6 +46,18 @@ typedef struct{
 	struct fcb* next;
 	struct fcb* prev;
 }fcb;
+
+/*Device*/
+typedef struct{
+	fcb* filehead;
+	byte* bitmap;
+	char devicename[NAME_LIMIT];
+	/*0 0 0 0 0 0 (Mounted) (Formated)*/
+	byte bits;
+}device;
+
+/*Device list*/
+device device_array [MAX_DEVICE];
 
 /*Open files array*/
 fcb open_files[MAX_OPEN];
