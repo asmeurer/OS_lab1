b0bb3e121070c6dba3add530c193d96c36dddcb2
Sheng Lundquist <slundqui007@gmail.com>
2012-04-24 21:13:41 -0600

    Oran needs shit

--- filesystem/file_manager.c
+++ filesystem/file_manager.c
@@ -6,3 +6,17 @@
  * Oran Wallace
  * Sheng Lundquist
  */
+
+int init_fs (){
+}
+
+int format(int device_num, char fs_name, int blocksize){
+	if(device_num < 0 || device_num >= MAX_DEVICE){
+		return ERROR_INVALID_DEVICE_NUM;
+	}
+	device format_me= device_array[device_num];
+	format_me.fs_name = fs_name;
+	format_me.blocksize = blocksize;
+	
+	
+}
